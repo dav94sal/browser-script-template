197fdb966113c5dadeb6712d475e4104ff11f419
David <dav94sal@gmail.com>
2025-02-21 14:57:17 -0600

    Remove initial data

--- data/job_seeking_data.py
+++
@@ -1,5 +0,0 @@
-job_urls = [
-    "https://huntr.co/track/goals/current",
-    "https://www.linkedin.com/jobs/?=",
-    "https://docs.google.com/document/d/1RMp9boo7_EUyrMBUJmr1lrDem3bN3XKktVLRq7DW3rI/edit?tab=t.0"
-]
